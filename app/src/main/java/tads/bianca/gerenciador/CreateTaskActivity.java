3b12e963bff90a1e21640d0c0bfafbd3b26a52e6
biancalr <33333728+biancalr@users.noreply.github.com>
2018-11-13 00:37:01 -0300

    Implementar o método para criar salvar as atividades

--- app/src/main/java/tads/bianca/gerenciador/CreateTaskActivity.java
+++ app/src/main/java/tads/bianca/gerenciador/CreateTaskActivity.java
@@ -1,16 +1,22 @@
 package tads.bianca.gerenciador;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class CreateTaskActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuthListener authListener;
+    private static final String TAG = "CreateTaskActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +24,13 @@ public class CreateTaskActivity extends AppCompatActivity {
         setContentView(R.layout.activity_creat_task);
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
+        this.mAuth = FirebaseAuth.getInstance();
+        this.authListener = new FirebaseAuthListener(this);
         Button buttonCreateTaskClick = (Button) findViewById(R.id.button_create_task);
         buttonCreateTaskClick.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                saveTask();
                 Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
                 Toast.makeText(CreateTaskActivity.this, "Activity created", Toast.LENGTH_SHORT).show();
                 startActivity(intent);
@@ -32,5 +41,12 @@ public class CreateTaskActivity extends AppCompatActivity {
                 R.array.spinner, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+
     }
+
+    private boolean saveTask(){
+        return true;
+    }
+
+
 }
